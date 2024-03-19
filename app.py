d73d3c065bf654a85b3320b79aaa24b2944771a7
Joaquin Vasquez <joaquinvasquez20@gmail.com>
2024-03-19 03:32:58 -0300

    se agrega pyinstaller para crear el ejecutable

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import re
 import csv
+import os
+import sys
 from airium import Airium
 from fractions import Fraction
+from datetime import datetime
 
-file = open("Solicitud de pedido.csv", encoding="utf-8")
+try:
+  file = open("Solicitud de pedido.csv", encoding="utf-8")
+except OSError:
+  print("No existe el archivo 'Solicitud de pedido.csv'")
+  input("Press RETURN...")
+  sys.exit()
+
 type(file)
 csvreader = csv.reader(file)
 header = []
@@ -38,111 +47,122 @@ with a.html(lang="es"):
       rel="stylesheet",
       href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap",
     )
-    # a.link(rel="stylesheet", href="style.css")
-    a.style(
-      _t="""
-      *,
-      *::before,
-      *::after {
-        box-sizing: border-box;
-      }
-      *::-webkit-scrollbar {
-        width: 12px;
-      }
-      *::-webkit-scrollbar-track {
-        background: #f0f0f0;
-        border: 2px solid transparent;
-        scrollbar-gutter: stable;
-      }
-      *::-webkit-scrollbar-thumb {
-        border-radius: 20px;
-        border: 2px solid transparent;
-        box-shadow: inset 0 0 10px 10px #aaa;
-      }
-      html {
-        background: #f0f0f0;
-      }
-      body {
-        font-family: 'Lato', sans-serif;
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100dvh;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-      h1 {
-        margin-top: 0;
-        color: #333;
-      }
-      body .table-container {
-        background: #fff;
-        max-height: 80vh;
-        width: 80%;
-        overflow-y: auto;
-      }
-      table {
-        width: 100%;
-        border-collapse: collapse;
-        table-layout: fixed;
-        border: 2px solid #333;
-      }
-      table th {
-        background-color: lightsteelblue;
-        border: 2px solid #333;
-        padding: 10px 0;
-      }
-      table td {
-        border: 1px solid #333;
-      }
-      table .name {
-        width: 200px;
-      }
-      table .quantity,
-      table .value {
-        width: 100px;
-      }
-      table .number {
-        text-align: center;
-      }
-      table .client-name {
-        background: lightblue;
-        overflow: hidden;
-        padding-left: 10px;
-      }
-      table .client-info {
-        padding-left: 10px;
-      }
-      table .sub-total,
-      table .total {
-        text-align: right;
-        padding-right: 10px;
-      }
-      table .sub-total-value {
-        font-weight: bold;
-        text-align: center;
-      }
-      table .total {
-        font-weight: bold;
-      }
-      table .total-value {
-        background: lightblue;
-      }
-      table .separator {
-        height: 2px;
-        background: #333;
-      }
-      table .separator:last-child {
-        display: none;
-      }
-"""
-    )
+    a.link(rel="stylesheet", href="style.css")
+#     a.style(
+#       _t="""
+#       *,
+#       *::before,
+#       *::after {
+#         box-sizing: border-box;
+#       }
+#       *::-webkit-scrollbar {
+#         width: 12px;
+#       }
+#       *::-webkit-scrollbar-track {
+#         background: #f0f0f0;
+#         border: 2px solid transparent;
+#         scrollbar-gutter: stable;
+#       }
+#       *::-webkit-scrollbar-thumb {
+#         border-radius: 20px;
+#         border: 2px solid transparent;
+#         box-shadow: inset 0 0 10px 10px #aaa;
+#       }
+#       html {
+#         background: #f0f0f0;
+#       }
+#       body {
+#         font-family: 'Lato', sans-serif;
+#         margin: 0;
+#         padding: 0;
+#         width: 100%;
+#         height: 100dvh;
+#         display: flex;
+#         flex-direction: column;
+#         justify-content: center;
+#         align-items: center;
+#       }
+#       h1 {
+#         margin-top: 0;
+#         color: #333;
+#       }
+#       body .table-container {
+#         background: #fff;
+#         max-height: 80vh;
+#         width: 80%;
+#         overflow-y: auto;
+#       }
+#       table {
+#         width: 100%;
+#         border-collapse: collapse;
+#         table-layout: fixed;
+#         border: 2px solid #333;
+#       }
+#       table th {
+#         background-color: lightsteelblue;
+#         border: 2px solid #333;
+#         padding: 10px 0;
+#       }
+#       table td {
+#         border: 1px solid #333;
+#       }
+#       table .name {
+#         width: 200px;
+#       }
+#       table .quantity,
+#       table .value {
+#         width: 100px;
+#       }
+#       table .number {
+#         text-align: center;
+#       }
+#       table .client-name {
+#         background: lightblue;
+#         overflow: hidden;
+#         padding-left: 10px;
+#       }
+#       table .client-info {
+#         padding-left: 10px;
+#       }
+#       table .sub-total,
+#       table .total {
+#         text-align: right;
+#         padding-right: 10px;
+#       }
+#       table .sub-total-value {
+#         font-weight: bold;
+#         text-align: center;
+#       }
+#       table .total {
+#         font-weight: bold;
+#       }
+#       table .total-value {
+#         background: lightblue;
+#       }
+#       table .separator {
+#         height: 2px;
+#         background: #333;
+#       }
+#       table .separator:last-child {
+#         display: none;
+#       }
+#       @media print {
+#       body {
+#         page-break-after: always;
+#       }
+#       .title {
+#         display: none !important;
+#       }
+#       .table-container {
+#       max-height: none !important;
+#       }
+#     }
+# """
+#     )
 
   with a.body():
     with a.h1(klass="title"):
-      a("Comandas Reverdecer")
+      a(f"Comandas Reverdecer {datetime.now().strftime('%d/%m/%Y')}")
     with a.div(klass="table-container"):
       with a.table():
         with a.tr():
@@ -220,3 +240,5 @@ html_bytes = bytes(a)
 # print(html)
 with open("index.html", "wb") as f:
   f.write(bytes(html_bytes))
+
+os.system("start index.html")
